61fdfffedf4c8b4859485a416b9b95fb05255028
Oscar Saharoy <osaharoy@gmail.com>
2025-02-22 16:52:42 +0000

    got quotes

--- getquotes.py
+++ getquotes.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 import requests
+import bs4
+import json
 
-response = requests.get( "https://www.azquotes.com/top_quotes.html?p=2" )
-print(vars(response))
+quotes = []
+
+for page in range(1, 11):
+    response = requests.get(
+        f"https://www.azquotes.com/top_quotes.html?p={page}"
+    )
+    soup = bs4.BeautifulSoup(response.content, "html.parser")
+    for q in soup.select( ".list-quotes .wrap-block" ):
+        quote = q.select_one( "a.title" ).string
+        author = q.select_one( "div.author a" ).string
+        quotes.append( {
+            "quote": quote,
+            "author": author,
+        } )
+
+print( json.dumps(quotes, indent=4) )
+
+
