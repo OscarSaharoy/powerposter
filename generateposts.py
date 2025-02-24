d1baa398878b7fd4aaff2ba63bba20dd3ce4ba77
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2025-02-24 22:59:46 +0000

    posttemplate

--- generateposts.py
+++ generateposts.py
@@ -1,19 +1,22 @@
+#!/usr/bin/env python3
+
 import subprocess
 
 with open("text-template.svg") as f:
     text_template = f.read()
 
-def measure(text):
+
+def measure(text, query):
     populated_template = text_template.replace( "{{ text }}", text )
 
     output = subprocess.run(
-        [ "inkscape", "--pipe", "--query-width", "--query-id=test-text" ],
+        [ "inkscape", "--pipe", query, "--query-id=test-text" ],
         input=populated_template,
         capture_output=True,
         text=True,
     )
 
-    return output
+    return float(output.stdout)
 
-text = "test text"
-print( measure(text) )
+measure_width  = lambda text: measure( text, "--query-width" )
+measure_height = lambda text: measure( text, "--query-height" )
